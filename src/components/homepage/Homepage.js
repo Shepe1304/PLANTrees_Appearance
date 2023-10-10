32070f10dd2f165202181f000ba8bb4dbb175b8b
Quynh-Giang Ho <hqgiang1304@gmail.com>
2023-10-11 05:58:26 +0700

    Update Homepage.js

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,11 +1,24 @@
 import "./Homepage.css";
 import React, { useState } from "react";
 import hero from "../general-components/images/irene-davila-O1PHdGGcljQ-unsplash.jpg";
+import { useNavigate } from "react-router";
 
 const Homepage = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const navigate = useNavigate();
 
-  const HandleFileSubmit = (e) => {};
+  const GetStartedClicked = () => {
+    navigate("/area");
+  };
+
+  const NavigateTo = (id) => {
+    if (id == 1) {
+      navigate("/area");
+    } else if (id == 2) {
+      navigate("/myspace");
+    } else if (id == 3) {
+      navigate("/contact");
+    }
+  };
 
   return (
     <div className="homepage">
@@ -27,15 +40,22 @@ const Homepage = () => {
           <span style={{ color: "green" }}>PLANT</span>rees
         </div>
         <div className="homepage--who_we_are--description">
-          ... is website that provides insights into the tree distribution near
-          you, and suggest areas with potential for planting!
+          Trees are essential for a healthy planet, but we are losing them at an
+          alarming rate. We are here to help you plant more trees in your
+          community by providing nearest suitable areas to plant trees from your
+          selected location. It can also suggest specific tree types based on
+          the soil and weather conditions in your area!
         </div>
         <div className="homepage--who_we_are--why bold">
           <span style={{ color: "green" }}>WHY</span>, YOU ASK?
         </div>
         <div className="homepage--who_we_are--why_answer">
           Because we would love to accompany you on the journey to make our
-          planet more <span style={{ color: "green" }}>green!</span>
+          planet more <span style={{ color: "green" }}>green!</span>{" "}
+          <div style={{ margin: "15px auto", maxWidth: "1050px" }}>
+            Our mission is to create a greener, healthier planet by making it
+            easy for people to plant trees.
+          </div>
         </div>
       </div>
 
@@ -52,12 +72,11 @@ const Homepage = () => {
           </span>
         </div>
         <div className="homepage--what_to_do--options">
-          <div className="homepage--what_to_do--option">
+          {/* <div className="homepage--what_to_do--option">
             <div className="homepage--what_to_do--dark"></div>
             <div className="homepage--what_to_do--option_text">
               TREE LIBRARY
             </div>
-            {/* Should the user really see information about trees in general, or is that irrelevant? */}
             <div className="homepage--what_to_do--option_desc">
               Where you can see information about trees, share health records of
               trees you know, and see other users' posts and inputs.
@@ -71,18 +90,13 @@ const Homepage = () => {
             <div className="homepage--what_to_do--option_desc">
               See your past posts and tree health inputs.
             </div>
-          </div>
-          <div className="homepage--what_to_do--option">
-            <div className="homepage--what_to_do--dark"></div>
-            <div className="homepage--what_to_do--option_text">
-              MY TREE SPACE
-            </div>
-            <div className="homepage--what_to_do--option_desc">
-              Upload a photo and see potential spots for planting, as well as
-              tree and plant type suggestions.
-            </div>
-          </div>
-          <div className="homepage--what_to_do--option">
+          </div> */}
+          <div
+            className="homepage--what_to_do--option"
+            onClick={(e) => {
+              NavigateTo(1);
+            }}
+          >
             <div className="homepage--what_to_do--dark"></div>
             <div className="homepage--what_to_do--option_text">
               AREA SUGGESTIONS
@@ -92,9 +106,36 @@ const Homepage = () => {
               view.
             </div>
           </div>
+          <div
+            className="homepage--what_to_do--option"
+            onClick={(e) => {
+              NavigateTo(2);
+            }}
+          >
+            <div className="homepage--what_to_do--dark"></div>
+            <div className="homepage--what_to_do--option_text">
+              MY TREE SPACE
+            </div>
+            <div className="homepage--what_to_do--option_desc">
+              Upload a photo and see potential spots for planting, as well as
+              tree and plant type suggestions.
+            </div>
+          </div>
+          <div
+            className="homepage--what_to_do--option"
+            onClick={(e) => {
+              NavigateTo(3);
+            }}
+          >
+            <div className="homepage--what_to_do--dark"></div>
+            <div className="homepage--what_to_do--option_text">CONTACT</div>
+            <div className="homepage--what_to_do--option_desc">
+              Contact us for more information!
+            </div>
+          </div>
         </div>
         <div className="homepage--what_to_do--get_started">
-          <button>GET STARTED</button>
+          <button onClick={GetStartedClicked}>GET STARTED</button>
         </div>
       </div>
     </div>
